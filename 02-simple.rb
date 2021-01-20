8f68f44db72db3b9e79e9cdf879f2c8fc19f4e19
Daniel Garcia <pel_daniel@hotmail.com>
2021-01-20 18:34:27 +0000

    Add Boolean & LessThan

--- 02-simple.rb
+++ 02-simple.rb
@@ -64,6 +64,44 @@ class SimpleMultiply < Struct.new(:left, :right)
   end
 end
 
+class SimpleBoolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class SimpleLessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      SimpleLessThan.new(left.reduce, right)
+    elsif right.reducible?
+      SimpleLessThan.new(left, right.reduce)
+    else
+      SimpleBoolean.new(left.value < right.value)
+    end
+  end
+end
+
 class SimpleMachine < Struct.new(:expression)
   def step
     self.expression = expression.reduce
@@ -79,15 +117,25 @@ class SimpleMachine < Struct.new(:expression)
   end
 end
 
+# SimpleMachine.new(
+#   SimpleAdd.new(
+#     SimpleMultiply.new(
+#       SimpleNumber.new(1),
+#       SimpleNumber.new(2)
+#     ),
+#     SimpleMultiply.new(
+#       SimpleNumber.new(3),
+#       SimpleNumber.new(4)
+#     )
+#   )
+# ).run
+
 SimpleMachine.new(
-  SimpleAdd.new(
-    SimpleMultiply.new(
-      SimpleNumber.new(1),
+  SimpleLessThan.new(
+    SimpleNumber.new(5),
+    SimpleAdd.new(
+      SimpleNumber.new(2),
       SimpleNumber.new(2)
-    ),
-    SimpleMultiply.new(
-      SimpleNumber.new(3),
-      SimpleNumber.new(4)
     )
   )
 ).run

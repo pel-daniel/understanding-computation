baeb30993fa020ab84968d58c37454de69f35075
Daniel Garcia <pel_daniel@hotmail.com>
2021-02-03 20:32:16 +0000

    Add free moves to NFA

--- 03-nondeterministic-finite-automata.rb
+++ 03-nondeterministic-finite-automata.rb
@@ -33,6 +33,16 @@ class NFARulebook < Struct.new(:rules)
       rule.applies_to?(state, character)
     end
   end
+
+  def follow_free_moves(states)
+    more_states = next_states(states, nil)
+
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
@@ -51,6 +61,10 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
     string.chars.each do |character|
       read_character(character)
     end
+  end
+
+  def current_states
+    rulebook.follow_free_moves(super)
   end
 end
 
@@ -72,17 +86,19 @@ end
 
 rulebook = NFARulebook.new(
   [
-    FARule.new(1, 'a', 1),
-    FARule.new(1, 'b', 1),
-    FARule.new(1, 'b', 2),
+    FARule.new(1, nil, 2),
+    FARule.new(1, nil, 4),
     FARule.new(2, 'a', 3),
-    FARule.new(2, 'b', 3),
-    FARule.new(3, 'a', 4),
-    FARule.new(3, 'b', 4)
+    FARule.new(3, 'a', 2),
+    FARule.new(4, 'a', 5),
+    FARule.new(5, 'a', 6),
+    FARule.new(6, 'a', 4)
   ]
 )
 
-nfa_design = NFADesign.new(1, [4], rulebook)
-puts nfa_design.accepts?('bab')
-puts nfa_design.accepts?('bbbbb')
-puts nfa_design.accepts?('bbabb')
+nfa_design = NFADesign.new(1, [2, 4], rulebook)
+
+puts nfa_design.accepts?('aa')
+puts nfa_design.accepts?('aaa')
+puts nfa_design.accepts?('aaaaa')
+puts nfa_design.accepts?('aaaaaa')
